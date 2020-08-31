853a31201645df335857ebd2759b7717491c2181
Lauren Dove <elledoveinc@gmail.com>
2020-08-31 10:47:47 -0400

    installed gem rac cors and set up event listener for when DOM is loaded

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("This is inside of index.js");
+const questionsurl = "http://localhost:3000/api/questions"
+
+document.addEventListener('DOMContentLoaded',() => {
+    console.log("Dom Loaded successfully")
+    fetch(questionsurl)
+    .then( response => response.json())
+    .then(question => {
+        console.log(question)
+
+    })
+
+})
