bbf88f7c0b784738963f53ba8c796cb73f222de2
Lauren Dove <elledoveinc@gmail.com>
2020-08-31 17:54:45 -0400

    added a fetch request to get questions from api. front and back now connected.

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const questionsurl = "http://localhost:3000/api/questions"
 
 document.addEventListener('DOMContentLoaded',() => {
-    console.log("Dom Loaded successfully")
-    fetch(questionsurl)
-    .then( response => response.json())
-    .then(question => {
-        console.log(question)
+    getQuestions();
+//     console.log("Dom Loaded successfully")
+//     fetch(questionsurl)
+//     .then( response => response.json())
+//    .then(data => {
+//         console.log(data)
 
-    })
-
+//     })
+        
 })
+
+
+ function getQuestions() {
+     fetch(questionsurl)
+    .then(response => response.json())
+     .then(questions => {
+      questions.data.forEach(question => {
+         const questionlist = 
+         `<div data-id=${question.id}> 
+         <h4>${question.attributes.a_question} </h4>
+         <p>${question.attributes.theme.name} </p>  
+         </div>
+         <br></br>`;
+         document.querySelector('#questions-container').innerHTML += questionlist;
+
+      })
+
+     })
+
+
+
+ }
